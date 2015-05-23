aeb1d05e2710a942ecd461fe45dc462a099b33d4
Capi Etheriel <barraponto@gmail.com>
2015-05-23 16:19:02 -0300

    [vejasp] visit every item to get more data

--- comebebesp/spiders/vejasp.py
+++ comebebesp/spiders/vejasp.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+from urlparse import urljoin
 import scrapy
+
 from comebebesp.items import ComebebespItemLoader
 
 class VejaspItemLoader(ComebebespItemLoader):
@@ -9,28 +11,47 @@ class VejaspItemLoader(ComebebespItemLoader):
 class VejaspSpider(scrapy.Spider):
     name = "vejasp"
     allowed_domains = ["vejasp.abril.com.br"]
-    urltemplate = ('http://vejasp.abril.com.br/estabelecimento/busca'
+    root_url = 'http://vejasp.abril.com.br/'
+    url_template = ('http://vejasp.abril.com.br/estabelecimento/busca'
                    '?per_page=20&page={page}&q=&fq=Restaurantes'
                    '&bairro=&nome=&preco_maximo=')
 
 
     def start_requests(self):
-        return [scrapy.Request(self.urltemplate.format(page=1),
+        return [scrapy.Request(self.url_template.format(page=1),
                                meta={'page': 1})]
 
     def parse(self, response):
         establishments = response.css('.map-list-item')
         for establishment in establishments:
+            item_url = establishment.css(
+                '.establishment-name a::attr("href")').extract()[0]
+
             loader = VejaspItemLoader(selector=establishment)
             loader.add_css('name', '.establishment-name::text')
-            loader.add_css('address', '.establishment-details p:first-child *::text')
-            loader.add_css('neighborhood', '[data-filtered-search-filter="bairro"]::text')
+            loader.add_css('address',
+                           '.establishment-details p:first-child *::text')
+            loader.add_css('neighborhood',
+                           '[data-filtered-search-filter="bairro"]::text')
             loader.add_xpath('city',
-                             establishment._css2xpath('[data-filtered-search-filter="bairro"]') + '/following-sibling::text()')
+                             '{xpath}/following-sibling::text()'.format(
+                                 xpath=establishment._css2xpath(
+                                     '[data-filtered-search-filter="bairro"]')))
             loader.add_xpath('telephone',
-                             establishment._css2xpath('.label') + '/following-sibling::text()')
-            loader.add_css('category', '[data-filtered-search-filter="especialidades"]::text')
-            yield loader.load_item()
+                             '{xpath}/following-sibling::text()'.format(
+                                 xpath=establishment._css2xpath('.label')))
+            loader.add_css('category',
+                           ('[data-filtered-search-filter="especialidades"]'
+                            '::text'))
+
+            yield scrapy.Request(urljoin(self.root_url, item_url),
+                                 callback=self.parse_item,
+                                 meta={'item': loader.load_item()})
+
         if establishments:
             next_page = response.meta['page'] + 1
-            yield scrapy.Request(self.urltemplate.format(page=next_page), meta={'page': next_page})
+            yield scrapy.Request(self.url_template.format(page=next_page), meta={'page': next_page})
+
+    def parse_item(self, response):
+        return response.meta['item']
+
